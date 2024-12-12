1d6ede313a200f242464d92b9cf1b90c220cabc8
CH4MI-MD <chamindul984@gmail.com>
2024-12-13 03:11:52 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,14 +17,13 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *CH4MI-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/CH4MI-MD_
+❒ *Author:* _wa.me/94703973470_
 └────────────⭓
 `
 
